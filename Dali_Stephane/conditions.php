e523edb1198c108d5f23d2f97ba473fea01eb001
Anouchka25 <informaticaux@gmail.com>
2021-11-23 20:59:59 +0100

    modifs

--- Dali_Stephane/conditions.php
+++ Dali_Stephane/conditions.php
@@ -7,6 +7,27 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Conditions en PHP</h1>
+<h1>Conditions en PHP</h1>
+    <form action="" method="post">
+        <fieldset>
+            <legend>Tu peux voter ou pas, allons-y vérifions</legend>
+            <input type="number" name="age" placeholder = "Votre age ?">
+            <input type="submit" value="Valider">
+        </fieldset>
+    </form>
+
+    <?php
+
+    //$age = isset($_POST['age']) ? $_POST['age'] : NULL; 
+
+    $age = 23;
+
+    if($age>=18){
+        echo "Vous êtes majeur pour voter";
+    }
+    else{ 
+        echo "Oh désolé, vous êtes encore mineur pour voter";
+    }
+    ?>
 </body>
 </html>
